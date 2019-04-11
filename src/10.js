3f8127d8138ad35e2a2c22f1a6b5bf7ed94785b7
Emanuelle Costa <eme@vonlou.dev>
2019-04-10 21:29:07 -0300

    Destructuring

---
+++ src/10.js
@@ -0,0 +1,17 @@
+// Destructuring
+
+let contact = {
+    fullName: "Shield Pack",
+    firstName: "Shield",
+    lastName: "Pack",
+    email: "shield@test.com",
+    age: 1
+};
+
+let {lastName, age} = contact;
+
+
+console.log(lastName, age);
+
+console.log(contact);
+
