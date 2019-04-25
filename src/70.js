3528df30897993b83a25cf3ab48b2bef90e55f56
Emanuelle Costa <eme@vonlou.dev>
2019-04-24 21:04:40 -0300

    ReverseString

--- src/70.js
+++ src/70.js
@@ -4,6 +4,10 @@ function convertToF(celsius) {
     return (celsius * (9 / 5)) + 32;
 }
 
+function convertToC(fahrenheit) {
+  return ((farenheit -32)/ 5 / 9);
+}
+
 // const convertToF = (celsius) => (celsius * (9 / 5)) + 32;
 
 
---
+++ src/71.js
@@ -0,0 +1,11 @@
+// freecodecamp
+
+function reverseString(str) {
+  let novaString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    novaString = novaString + str[i];
+  }
+  return novaString;
+}
+
+console.log(reverseString("hello"));
