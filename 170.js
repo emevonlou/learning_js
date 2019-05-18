213f61ff25500370515a00963be9e75d74d93bea
Emanuelle Costa <eme@vonlou.dev>
2019-05-18 17:23:35 -0300

    WeakSet

---
+++ 170.js
@@ -0,0 +1,18 @@
+// WeakSet
+
+let obj1 = {
+    name: 'Shield',
+    age: 2
+};
+
+let obj2 = {
+    name: 'Louise',
+    age: 2
+};
+
+let eme = new WeakSet([obj1, obj2]);
+
+console.log(eme.has(obj1));
+console.log(eme.has(obj2));
+
+
