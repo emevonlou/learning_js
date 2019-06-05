47f5c58b19a5ebb8e5d19e289af84a6b05d62264
Emanuelle Costa <eme@vonlou.dev>
2019-06-05 20:19:48 -0300

    Printing steps!

--- src/177.js
+++ src/177.js
@@ -15,3 +15,28 @@ console.log(animalsMap.size);
 console.log(animalsMap.has('llama'));
 console.log(animalsMap.has('lion'));
 console.log(animalsMap.get('alpaca'));
+
+console.log('.....................................\n');
+
+const children = {
+    fun: 4,
+    focused: 5,
+    spontaneous: 2,
+    timid: 3
+}
+
+console.log(Object.entries(children));
+
+const childrenMap = new Map(Object.entries(children));
+
+console.log(childrenMap.size);
+
+console.log(childrenMap.get('fun'));
+console.log(childrenMap.get('timid'));
+console.log(childrenMap.get('fun'));
+console.log(childrenMap.get('focused'));
+
+console.log(childrenMap.has('fun'));
+console.log(childrenMap.has('focused'));
+console.log(childrenMap.has('spontaneous'));
+console.log(childrenMap.has('timid'));
---
+++ src/194.js
@@ -0,0 +1,24 @@
+// Printing steps
+
+function steps(x) {
+    // From 0 to x(iterate through rows)
+    for (let role = 0; role < x; role++) {
+        // create empty string 'escada'
+        let escada = '';
+        // From 0 to x(iterate through columns)
+        for (let coluna = 0; coluna < x; coluna++) {
+            // If the current coluna is equal to or less than the current role
+            // add a '#' to 'escada'
+            if(coluna <= role) {
+                escada += '#';
+            } else {
+                escada += ' ';
+            }
+        }
+
+        console.log(escada);
+    }
+
+}
+
+console.log(steps(30));
