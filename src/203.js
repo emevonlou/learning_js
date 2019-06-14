213cf6d39ee9954e74931e530e79bbcf3f21f7ef
Emanuelle Costa <eme@vonlou.dev>
2019-06-14 19:29:34 -0300

    Step Back From the Tail!

---
+++ src/203.js
@@ -0,0 +1,17 @@
+
+function fromLast(list, x) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(x > 0) {
+        fast = fast.next;
+        x--;
+    }
+
+    while(fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
