d6b60937c2c9092892b8642f9f401cfba0ab9544
Emanuelle Costa <eme@vonlou.dev>
2019-06-21 18:46:46 -0300

    MergeSort!

--- src/209.js
+++ src/209.js
@@ -30,9 +30,31 @@ function selectionSort(arr) {
             arr[i] = lesser;
         }
     }
-
+    return arr;
 }
 
 function mergeSort(arr) {
-    // see here tomorrow!
+    if(arr.length === 1) {
+        return arr;
+    }
+
+    const center= Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    const results = [];
+
+    while(left.length && right.length) {
+        if(left[0] < right[0]) {
+            results.push(left.shift());
+        }else {
+            results.push(right.shift());
+        }
+    }
+
+    return[...results, ...left, ...right];
 }
